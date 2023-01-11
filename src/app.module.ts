fb502afcbf8bebcb1d2c8f1d2aecd5d16aea6b2f
KENKEN <jhl0803@naver.com>
2023-01-11 17:58:26 +0900

    kafka 연동 셋팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_CLIENT_MODULE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'tester1',
+            brokers: ['localhost:29092'],
+          },
+          consumer: {
+            groupId: 'test-group-id',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const kafka = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:29092'],
+        },
+      },
+    },
+  );
+  await kafka.listen();
 }
-bootstrap();
+
+bootstrap().then(() => console.log('Application is running on: 3000'));
