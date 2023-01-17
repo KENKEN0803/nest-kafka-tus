c6ab71f359390f1dd6a511876ab47ffc19250f01
KENKEN <jhl0803@naver.com>
2023-01-17 16:12:09 +0900

    압축해제 윈도우 처리 추가

--- src/shell/shell.service.ts
+++ src/shell/shell.service.ts
@@ -8,29 +8,44 @@ export class ShellService {
 
   execUnzip(fileName: string): Promise<number> {
     this.logger.log(`Executing unzip.... ${fileName}`);
-    const execArgs = [
-      '-q', // quiet
-      '-o', // overwrite
-      '-d', // destination
-      storageConfig.unzipOutputPath,
-      `${storageConfig.uploadFileStoragePath}/${fileName}`,
-    ];
+    let command;
+    let execArgs;
+
+    if (process.platform !== 'win32') {
+      command = 'unzip';
+      execArgs = [
+        '-q', // quiet
+        '-o', // overwrite
+        '-d', // destination
+        storageConfig.unzipOutputPath,
+        `${storageConfig.uploadFileStoragePath}/${fileName}`,
+      ];
+    } else {
+      command = 'tar';
+      execArgs = [
+        '-xvf',
+        `${storageConfig.uploadFileStoragePath}/${fileName}`,
+        '-C',
+        storageConfig.unzipOutputPath,
+      ];
+    }
+
     return new Promise((resolve, reject) => {
-      const unzip = spawn('unzip', execArgs);
+      const unzip = spawn(command, execArgs);
 
       // unzip.stdout.on('data', (data) => {
       //   console.log(`stdout: ${data}`);
       // });
 
-      unzip.stderr.on('data', (data) => {
-        this.logger.error(`unzip stderr: ${data}`);
-        reject(`unzip stderr: ${data}`);
-      });
+      // unzip.stderr.on('data', (data) => {
+      //   this.logger.error(`unzip stderr: ${data}`);
+      //   reject(`unzip stderr: ${data}`);
+      // });
 
-      unzip.on('error', (error) => {
-        this.logger.error(`unzip error: ${error.message}`);
-        reject(`unzip error: ${error.message}`);
-      });
+      // unzip.on('error', (error) => {
+      //   this.logger.error(`unzip error: ${error.message}`);
+      //   reject(`unzip error: ${error.message}`);
+      // });
 
       // unzip.on('close', (code) => {
       //   console.log('close');
@@ -44,15 +59,15 @@ export class ShellService {
           );
           resolve(code);
         } else {
-          this.logger.error(`unzip process exited with code ${code}`);
-          reject(`unzip process exited with code ${code}`);
+          this.logger.error(`unzip process fail with code ${code}`);
+          reject(`unzip process exited fail code ${code}`);
         }
       });
 
-      unzip.on('disconnect', (code: number) => {
-        this.logger.error(`unzip process disconnected with code ${code}`);
-        reject(`unzip process disconnected with code ${code}`);
-      });
+      // unzip.on('disconnect', (code: number) => {
+      //   this.logger.error(`unzip process disconnected with code ${code}`);
+      //   reject(`unzip process disconnected with code ${code}`);
+      // });
     });
   }
 
